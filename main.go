9ff990cef129cef5d0ca4b02969f6993e8321e59
Nao YONASHIRO <owan.orisano@gmail.com>
2018-03-02 01:12:58 +0900

    wip: PoC

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template/parse"
+)
+
+func skipToken(b []byte) []byte {
+	return b[6:]
+}
+
+func main() {
+	forest, err := parse.Parse("twsql", `SELECT * FROM foo WHERE username = /* .UserName */'john'`, "/*", "*/")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tree := forest["twsql"]
+	skip := false
+	for _, node := range tree.Root.Nodes {
+		switch node := node.(type) {
+		case *parse.TextNode:
+			t := node.Text
+			if skip {
+				t = skipToken(t)
+				skip = false
+			}
+			os.Stdout.Write(t)
+		case *parse.ActionNode:
+			skip = true
+			os.Stdout.Write([]byte{'?'})
+		}
+	}
+	// pp.Println(tree.Root.Nodes)
+}
